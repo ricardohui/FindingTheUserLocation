c203d1f39457f0367eb9b0d172fed2f24421c166
Ricardo Hui <eehaha@gmail.com>
2019-03-27 07:30:02 +0800

    working

--- FindingTheUserLocation/ViewController.swift
+++ FindingTheUserLocation/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import CoreLocation
+import MapKit
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate{
+    var locationManager = CLLocationManager()
+    
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self;
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+        let userLocation: CLLocation = locations[0]
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        let latDelta : CLLocationDegrees = 0.05
+        let lonDelta : CLLocationDegrees  = 0.05
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let region = MKCoordinateRegion(center: location, span: span)
+        self.map.setRegion(region, animated: true)
+        
+    }
 
 }
 
