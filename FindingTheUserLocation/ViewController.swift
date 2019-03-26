2f31484c175c9073f2d17b8ca576aedce08ed2a4
Ricardo Hui <eehaha@gmail.com>
2019-03-27 07:50:15 +0800

    grab the full address from geolocation

--- FindingTheUserLocation/ViewController.swift
+++ FindingTheUserLocation/ViewController.swift
@@ -25,17 +25,45 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print(locations)
+//        print(locations)
         let userLocation: CLLocation = locations[0]
-        let latitude = userLocation.coordinate.latitude
-        let longitude = userLocation.coordinate.longitude
-        let latDelta : CLLocationDegrees = 0.05
-        let lonDelta : CLLocationDegrees  = 0.05
-        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
-        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        let region = MKCoordinateRegion(center: location, span: span)
-        self.map.setRegion(region, animated: true)
-        
+     
+        CLGeocoder().reverseGeocodeLocation(userLocation) { (placemarks, error) in
+            if error != nil {
+                print(error)
+            }else{
+                if let placemark = placemarks?[0]{
+                    print(placemark)
+                    var subThroughfare = ""
+                    if placemark.subThoroughfare != nil{
+                        subThroughfare = placemark.subThoroughfare!
+                    }
+                    var thoroughfare = ""
+                    if placemark.thoroughfare != nil{
+                        thoroughfare = placemark.thoroughfare!
+                    }
+                    var subLocality = ""
+                    if placemark.subLocality != nil {
+                        subLocality = placemark.subLocality!
+                    }
+                    var subAdministrativeArea = ""
+                    if placemark.subAdministrativeArea  != nil {
+                        subAdministrativeArea = placemark.subAdministrativeArea!
+                    }
+                    var postalCode = ""
+                    if placemark.postalCode  != nil {
+                        postalCode = placemark.postalCode!
+                    }
+                    var country = ""
+                    if placemark.country  != nil {
+                        country = placemark.country!
+                    }
+                    
+                    let addressString  = subThroughfare + " " + thoroughfare + "\n" + subLocality + subAdministrativeArea + "\n" + postalCode + "\n" + country
+                    print(addressString)
+                }
+            }
+        }
     }
 
 }
